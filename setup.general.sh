909a6b0951c357ba019f705eedcdf67f5944258b
Ziv Peleg <ziv@bugatone.com>
2017-08-27 16:53:57 +0300

    Added cmake installation and some missing python packages

--- setup.general.sh
+++ setup.general.sh
@@ -1,15 +1,18 @@
 echo "Linux setup - general, ALSA Support and Python"
 sudo apt-get update
 sudo apt-get upgrade -y
+
 echo "general installations"
 sudo apt-get install subversion vim libfftw3-dev -y
 sudo apt-get install libatlas3-base libatlas-dev libatlas-base-dev libblas-dev liblapack-dev gfortran -y
+
 echo "ALSA installations"
 sudo apt-get install libasound2-dev libncurses5-dev libncursesw5 libncursesw5-dev xmlto -y
 sudo apt-get install python-pip python-dev -y
 sudo apt-get install dvipng -y
 sudo apt-get install idle -y
 sudo apt-get install libhdf5-serial-dev -y
+
 echo "python installations"
 sudo pip install --upgrade pip
 sudo pip install numpy
@@ -26,4 +29,13 @@ sudo pip install tables
 sudo pip install pandas
 sudo pip install symfit
 sudo pip install pyusb==1.0.0b2 
+sudo pip install flask
+sudo pip install cbor
+sudo pip install boto3
 sudo pip install pygithub
+
+echo "cmake install"
+wget https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
+mv cmake-3.9.1-Linux-x86_64.tar.gz /opt/cmake.tar.gz
+tar -xzf /opt/cmake.tar.gz
+mv /opt/cmake-3.9.1-Linux-x86_64 /opt/cmake
--- setup.git.sh
+++ setup.git.sh
@@ -1,12 +1,13 @@
 echo "setup git"
 echo "checkout code"
 git clone https://github.com/Bugatone/Bugatone-Space.git bugatone-space
-echo "set path follwing:"
-echo "export BUGATONE_ROOT=/home/buga/bugatone-space" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=.:/home/buga/bugatone-space/lib/linux_x86:$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export PATH=.:/home/buga/bugatone-space/bin/linux_x86:$PATH" >> ~/.bashrc
-echo "export PYTHONPATH=/home/buga/bugatone-space/python" >> ~/.bashrc
-source ~/.bashrc
+echo "Updating .profile file"
+echo "export BUGATONE_ROOT=/home/buga/bugatone-space" >> ~/.profile
+echo "export LD_LIBRARY_PATH=.:/home/buga/bugatone-space/lib/linux_x86:$LD_LIBRARY_PATH" >> ~/.profile
+echo "export PATH=.:/home/buga/bugatone-space/bin/linux_x86:$PATH" >> ~/.profile
+echo "export PYTHONPATH=/home/buga/bugatone-space/python" >> ~/.profile
+echo "export PATH=/opt/cmake/bin:$PATH" >> ~/.profile
+source ~/.profile
 echo "make project"
 make all -C ~/bugatone-space/
 
