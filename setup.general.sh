69c0745ec5097bb6066555923e8cefe38ff64482
zpeleg <ziv@bugatone.com>
2017-09-27 19:27:22 +0300

    More missing pip modules

--- setup.general.sh
+++ setup.general.sh
@@ -34,6 +34,8 @@ sudo pip install cbor
 sudo pip install boto3
 sudo pip install pygithub
 sudo pip install subprocess32
+sudo pip install twisted
+sudo pip install requests
 
 echo "cmake install"
 wget https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
