88aa270177a39291e1d3e00056f8eef118a42f65
zpeleg <ziv@bugatone.com>
2017-09-27 20:48:00 +0300

    More missing packages

--- setup.general.sh
+++ setup.general.sh
@@ -36,6 +36,7 @@ sudo pip install pygithub
 sudo pip install subprocess32
 sudo pip install twisted
 sudo pip install requests
+sudo pip install bs4
 
 echo "cmake install"
 wget https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
