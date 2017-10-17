adbab304e69bffe188e6c763aaf93d9afc45222c
Ziv Peleg <ziv@bugatone.com>
2017-10-17 11:31:33 +0300

    Added post install steps to readme and fixed .profile file

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Just copy these 3 lines to your terminal and run :)
 ```bash
 sudo apt-get install git && git clone https://github.com/Bugatone/Ubuntu-Setup-Scripts.git setup-scripts && cd setup-scripts && ./install.sh
 ```
+
+Stuff to do after installing if needed:
+1. Make sure you can compile the code via ./make.sh
+2. Using jetbrains toolbox install CLion and PyCharm
+3. Install Android Studio
+4. Install ndk
+5. Install android sdk via studio in all the required version (Build tool 24, api level 23, plus the latest version of adb)
+6. Make sure you can compile BugatoneAudioServerService in android studio
+
--- setup.git.sh
+++ setup.git.sh
@@ -5,12 +5,13 @@ cd
 git clone https://github.com/Bugatone/Bugatone-Space.git
 ln -s Bugaton-Space git
 echo "Updating .profile file"
-echo "export BUGATONE_ROOT=/home/buga/bugatone-space" >> ~/.profile
-echo "export LD_LIBRARY_PATH=.:/home/buga/bugatone-space/lib/linux_x86:$LD_LIBRARY_PATH" >> ~/.profile
-echo "export PATH=.:/home/buga/bugatone-space/bin/linux_x86:$PATH" >> ~/.profile
-echo "export PYTHONPATH=/home/buga/bugatone-space/python" >> ~/.profile
-echo "export PATH=/opt/cmake/bin:$PATH" >> ~/.profile
+echo "export BUGATONE_ROOT=/home/buga/Bugatone-Space" >> ~/.profile
+echo "export LD_LIBRARY_PATH=.:/home/buga/Bugatone-Space/lib/linux_x86:\$LD_LIBRARY_PATH" >> ~/.profile
+echo "export PATH=.:/home/buga/Bugatone-Space/bin/linux_x86:\$PATH" >> ~/.profile
+echo "export PYTHONPATH=/home/buga/Bugatone-Space/python" >> ~/.profile
+echo "export PATH=/opt/cmake/bin:\$PATH" >> ~/.profile
 source ~/.profile
 echo "make project"
-make all -C ~/bugatone-space/
+cd Bugatone-Space
+./make.sh linux
 
