6535295ac6eb59e3466ea9bc6342422ee908ec95
Ziv Peleg <ziv@bugatone.com>
2017-10-18 11:59:16 +0300

    Typo

--- setup.git.sh
+++ setup.git.sh
@@ -4,7 +4,7 @@ echo "checkout code"
 cd
 git clone https://github.com/Bugatone/Bugatone-Space.git
 sudo chown -R buga:buga Bugatone-Space
-ln -s Bugaton-Space git
+ln -s Bugatone-Space git
 echo "Updating .profile file"
 echo "export BUGATONE_ROOT=/home/buga/Bugatone-Space" >> ~/.profile
 echo "export LD_LIBRARY_PATH=.:/home/buga/Bugatone-Space/lib/linux_x86:\$LD_LIBRARY_PATH" >> ~/.profile
