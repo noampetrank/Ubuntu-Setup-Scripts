991fbe7af03c622494e7812868092592fab23c12
Ziv Peleg <ziv@bugatone.com>
2017-11-19 18:58:40 +0200

    Added the needed bugaconfigs

--- setup.buga.directories.sh
+++ setup.buga.directories.sh
@@ -4,4 +4,6 @@ mkdir ~/experiments
 mkdir ~/experiments/parameters
 mkdir ~/tmp_dir
 mkdir ~/buga_logs
-sudo chown -R buga:buga ~/experiments ~/tmp_dir ~/buga_logs
+cp bugaconfig.ini /home/buga/
+cp bugaconfig.ini /root/
+sudo chown -R buga:buga ~/experiments ~/tmp_dir ~/buga_logs /root/bugaconfig.ini /home/buga/bugaconfig.ini
