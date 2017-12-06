1badc205967bf5e5614ff40c43636420fd86f55c
Dror Speiser <dror.mastershin@gmail.com>
2017-12-06 11:14:00 +0200

    added stopit module

--- setup.python.libs.sh
+++ setup.python.libs.sh
@@ -26,6 +26,7 @@ sudo pip install subprocess32
 sudo pip install twisted
 sudo pip install requests
 sudo pip install bs4
+sudo pip install stopit
 
 # Something that's required for the demo guis.
 apt install python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0
