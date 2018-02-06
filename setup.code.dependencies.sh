83d3239eae8d7c458784975135387e4569c629f9
Ziv Peleg <ziv@bugatone.com>
2018-02-06 19:30:16 +0200

    Fix what I broke and add htop

--- setup.code.dependencies.sh
+++ setup.code.dependencies.sh
@@ -4,13 +4,13 @@ echo "Installing dependencies"
 sudo apt-get install vim libfftw3-dev libatlas3-base libatlas-dev libatlas-base-dev libblas-dev liblapack-dev gfortran python-gtk2 -y
 
 echo "Installing cmake"
-#wget https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
-#tar -xzf cmake-3.9.1-Linux-x86_64.tar.gz -C /opt
-#mv /opt/cmake-3.9.1-Linux-x86_64 /opt/cmake
+wget https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
+tar -xzf cmake-3.9.1-Linux-x86_64.tar.gz -C /opt
+mv /opt/cmake-3.9.1-Linux-x86_64 /opt/cmake
 
 echo "Installing vs code"
-#wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb
-#dpkg -i vscode.deb
-#apt-get install -f
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb
+dpkg -i vscode.deb
+apt-get install -f
 
-sudo apt-get install meld gitk -y
+sudo apt-get install meld gitk htop -y
