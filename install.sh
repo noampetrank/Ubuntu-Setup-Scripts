6105efc66834c49bbdcb349a993b4b9ca14ed761
Ziv Peleg <ziv@bugatone.com>
2018-03-08 16:36:36 +0200

    Install ssh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt-get upgrade -y
 # ./setup.net.sh
 ./setup.git.sh
 ./setup.buga.directories.sh
-# ./setup.openssh.sh
+./setup.openssh.sh
 ./setup.storage.sh
 ./setup.desktop.shortcuts.sh
 ./setup.network.workaround.sh
