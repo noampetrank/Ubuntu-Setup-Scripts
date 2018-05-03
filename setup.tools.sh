8153610f7f6065e7b31012ebbb261b6f23cf819b
Tzippy <tzippy@bugatone.com>
2018-05-03 12:30:21 +0300

    Added more tools
    emacs-mozc, gnome-tweak-tool, id3v2, keepassx, mpv, screen and tree

--- setup.tools.sh
+++ setup.tools.sh
@@ -14,3 +14,25 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo apt-get install -f
 sudo dpkg -i google-chrome*.deb
+
+echo "Install emacs-mozc"
+sudo apt-get update
+sudo apt-get install emacs-mozc
+
+echo "Install gnome-tweak-tool"
+sudo apt install gnome-tweak-tool
+
+echo "Install id3v2"
+sudo apt-get install id3v2
+
+echo "Install keepassx"
+sudo apt-get install keepassx
+
+echo "Install mpv"
+sudo apt install mpv
+
+echo "Install screen"
+sudo apt-get install screen
+
+echo "Install tree"
+sudo apt-get install tree
