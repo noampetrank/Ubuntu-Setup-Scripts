1d9e5b98fc8d0285cab96cf570f9ab0bb6551128
Tzippy <tzippy@bugatone.com>
2018-06-05 16:58:04 +0300

    Update setup.shortcuts.and.songs.sh

--- setup.shortcuts.and.songs.sh
+++ setup.shortcuts.and.songs.sh
@@ -3,5 +3,5 @@ echo "copy some shortcuts to desktop"
 cp ~/Bugatone-Space/python/scripts/shortcuts/* ~/Desktop/
 echo "copy songs folder from storage to home/buga"
 mkdir /home/buga/songs/
-rsync -avzhP /mnt/buga/storage/songs/ /home/buga/songs/
+rsync -avzhP /mnt/buga_storage/songs/ /home/buga/songs/
 
