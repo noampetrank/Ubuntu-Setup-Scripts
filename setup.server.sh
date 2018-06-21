d9af70d6d2a001f6b72539cf3d010ad99911cf5c
Yotam Karmon <yotam@bugatone.com>
2018-06-21 11:44:49 +0300

    Remove unneeded lines from server setup script

--- setup.server.sh
+++ setup.server.sh
@@ -11,10 +11,7 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main"
 sudo apt-get update
 sudo apt-get install -y clang-5.0
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-5.0 1000
-sudo update-alternatives --install /usr/bin/clang++ clang /usr/bin/clang-3.8 100
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-5.0 1000
 sudo update-alternatives --config clang --skip-auto
 sudo update-alternatives --config clang++ --skip-auto
