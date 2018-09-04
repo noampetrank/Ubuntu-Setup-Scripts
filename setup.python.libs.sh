28b02739b2dd7b604b8af502f6f0adc5193f1a3b
zpeleg <zivpeleg@gmail.com>
2018-09-04 11:19:08 +0300

    add sudo

--- setup.python.libs.sh
+++ setup.python.libs.sh
@@ -33,4 +33,4 @@ sudo -H pip install pathlib2
 sudo -H pip install jinja2
 
 # Something that's required for the demo guis.
-apt install python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0
+sudo apt install python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0
