29740c4c9fe494c1a5795322c112b9f272951bc6
zpeleg <zivpeleg@gmail.com>
2018-09-04 13:14:08 +0300

    Added new dependency

--- setup.python.libs.sh
+++ setup.python.libs.sh
@@ -31,6 +31,7 @@ sudo -H pip install colorama
 sudo -H pip install lxml
 sudo -H pip install pathlib2
 sudo -H pip install jinja2
+sudo -H pip install nose-exclude
 
 # Something that's required for the demo guis.
 sudo apt install python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0
