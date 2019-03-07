11d5beb41d907d28b30ff6693bfadfe80b32ceb2
Yotam Karmon <yotam@bugatone.com>
2019-03-07 16:28:41 +0200

    Fix install_cmake_from_apt.sh

--- install_cmake_from_apt.sh
+++ install_cmake_from_apt.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:janisozaur/cmake-update
+sudo add-apt-repository -y ppa:janisozaur/cmake-update
 sudo apt-get update
 sudo apt-get install -y cmake=3.12.1-1~16.04.1ubuntu1
 sed -i 's/export PATH=\/opt\/cmake\/bin\:\$PATH//g' ~/.profile
