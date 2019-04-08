ee7e9024748db7e73f16b232026cb49cbc4f3747
Ziv Peleg <zivpeleg@gmail.com>
2019-04-08 12:37:40 +0300

    More explanations and install cmake on 18.04

--- setup.ubuntu.18.sh
+++ setup.ubuntu.18.sh
@@ -11,8 +11,22 @@ sudo update-alternatives --set cc /usr/bin/gcc
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
 sudo update-alternatives --set c++ /usr/bin/g++
 
-# Not installing libatlas-dev as it was removed after 16.04, don't know if it will cause problems
+# Same as in setup.code.dependencies. Not installing libatlas-dev as it was removed after 16.04, don't know if it will cause problems
 sudo apt-get install vim libfftw3-dev libatlas3-base libatlas-base-dev libblas-dev liblapack-dev gfortran python-gtk2 default-jdk binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev default-jdk bbe fping -y
 
 # TODO: Installing cmake does not work, need to do it manually.
+# https://askubuntu.com/a/865294
+version=3.12
+build=1
+mkdir temp
+cd temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make -j4
+sudo make install
+cd ../..
+
+
 # TODO: Clang does not install all the way, not sure why. Running the commands manually worked.
