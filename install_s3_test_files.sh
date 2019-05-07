4de1403b5d3dcd720318e5f8cf252fd8bf729af0
Yotam Karmon <yotam@bugatone.com>
2019-05-07 14:16:40 +0300

    Add a script for installing AWS CLI and test-files from S3

---
+++ install_s3_test_files.sh
@@ -0,0 +1,8 @@
+sudo apt install -y awscli
+echo "export AWS_ACCESS_KEY_ID=AKIASMHZ4KIOX7Z7MXE5" >> ~/.profile
+echo "export AWS_SECRET_ACCESS_KEY=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" >> ~/.profile
+source ~/.profile
+cd
+mkdir test-files-new
+cd test-files-new
+aws s3 sync s3://buga-resources-test-files .
