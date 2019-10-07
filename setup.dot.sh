02d8f8164aeb791ae62a699846813740b6a63f0a
Dror Speiser <dror.mastershin@gmail.com>
2019-10-07 13:22:01 +0200

    Create setup.dot.sh

---
+++ setup.dot.sh
@@ -0,0 +1,11 @@
+sudo apt install byacc bison flex python3-dev
+mkdir -p /home/buga/.soft
+cd /home/buga/.soft
+curl https://gitlab.com/graphviz/graphviz/-/archive/master/graphviz-master.tar.gz --output graphviz-master.tar.gz
+tar -xzvf graphviz-master.tar.gz
+cd graphviz-master/
+./autogen.sh
+./configure --enable-perl=no
+make
+sudo make install
+pip install --user git+https://github.com/drorspei/sismic-viz --upgrade
