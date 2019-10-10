feb4c7da41eb91e6e78216d9852cc5a8697ac9c0
Ziv Peleg <zivpeleg@gmail.com>
2019-10-10 16:57:23 +0300

    Remove old cmake install script

--- install_cmake_from_apt.sh
+++
@@ -1,6 +0,0 @@
-sudo add-apt-repository -y ppa:janisozaur/cmake-update
-sudo apt-get update
-sudo apt-get install -y cmake=3.12.1-1~16.04.1ubuntu1
-sed -i 's/export PATH=\/opt\/cmake\/bin\:\$PATH//g' ~/.profile
-rm -rf 
-sudo rm -rf /opt/cmake
